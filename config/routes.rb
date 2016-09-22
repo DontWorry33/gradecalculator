b3c11e67179705818fd70e9d0e284fbf0b9783b4
Hussain Hassam <hussain.hassam21@gmail.com>
2016-09-21 17:48:31 -0700

    initial commit for assignment 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+
   resources :widgets
 
   # The priority is based upon order of creation: first created -> highest priority.
@@ -7,6 +9,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
 
   root 'welcome#index'
+
+  resources :asn1
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
